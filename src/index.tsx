de66a765ce198f1900dbbe5cdb2cf9c72a89b06d
Jacky Efendi <jacky.efendi1@gmail.com>
2020-08-05 20:29:36 +0700

    feat: setup css modules

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React, { FC, HTMLAttributes, ReactChild } from 'react';
 
+import styles from './index.module.css';
+
 export interface Props extends HTMLAttributes<HTMLDivElement> {
   children?: ReactChild;
 }
@@ -7,5 +9,6 @@ export interface Props extends HTMLAttributes<HTMLDivElement> {
 // Please do not use types off of a default export module or else Storybook Docs will suffer.
 // see: https://github.com/storybookjs/storybook/issues/9556
 export const Thing: FC<Props> = ({ children }) => {
-  return <div>{children || `the snozzberries taste like snozzberries`}</div>;
+  console.log({ styles })
+  return <div className={styles['red']}>{children || `the snozzberrasdsadies taste like snozzberries`}</div>;
 };
